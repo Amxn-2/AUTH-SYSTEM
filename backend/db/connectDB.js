34473ab3d9fd031f190b49f151d66364b5257664
Amxn-2 <amansharmaa1205@gmail.com>
2024-10-20 09:46:51 +0530

    Initial commit for authentication system project

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -4,7 +4,7 @@ export const connectDB = async () => {
       try {
             console.log("mongo_uri", process.env.MONGO_URI);
             const conn = await mongoose.connect(process.env.MONGO_URI)
-            // console.log(conn);
+            console.log(conn);
             console.log(`MongoDB Connected: ${conn.connection.host}`);
       } catch (error) {
             console.log("Error to connect,", error.message)
